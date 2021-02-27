4bf462a7f1a6bff507a093d05900ee57a598854e
gouri-panda <gouripanda4@gmail.com>
2021-02-27 18:19:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/forgedapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.forgedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
